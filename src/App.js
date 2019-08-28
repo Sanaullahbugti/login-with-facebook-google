95186d20675926ca574b740e347490f983480e24
Sanaullahbugti <sanaullahbugti821@gmail.com>
2019-08-28 20:36:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import FacebookLogin from 'react-facebook-login';
+
+import GoogleLogin from 'react-google-login';
+
+class App extends Component {
+  state = {
+    user: null
+  }
+  responseFacebook = (response) => {
+    this.setState({
+      user: response
+    })
+  }
+  render() {
+
+    const { user } = this.state
+    console.log(user)
+
+    return (
+      <div className="App">
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+        <FacebookLogin
+          appId="1801834103451410"
+          fields="name,email,picture"
+          callback={this.responseFacebook}
+        />
+        <br />
+        <br />
+        {user !== null && 
+        <div><img src={user.picture.data.url} 
+        alt="no profile pic" 
+        style={{ width: "100px", height: "100px" }} 
+        />
+        <h4>you are login as {user.name}</h4>
+        </div>}
+      </div>
+    );
+  }
+}
+
+export default App;
